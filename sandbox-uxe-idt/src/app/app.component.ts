e0976dca0de2b0e7fa9c7b0080bb58e64b432ac6
PabloSanchezLeyTR <pablo.sanchez2@thomsonreuters.com>
2024-10-24 11:23:13 -0600

    starting project

---
+++ sandbox-uxe-idt/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SafButton } from '@saffron/core-components';
+
+
+SafButton();
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'sandbox-uxe-idt';
+}
